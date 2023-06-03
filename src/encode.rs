5124abdcb40631f18d5e62a6fe3e3a3ccb87c4b3
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2023-06-03 00:24:13 -0300

    Added concurrency to decode and encode the music.
    Renamed music.rs to decode.rs and created an encode.rs file to handle encoding into opus before sending the data over reqwest to the server.

---
+++ src/encode.rs
@@ -0,0 +1,5 @@
+
+
+pub async fn encode_music(){
+	println!("encodin!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::fs::File;
+	
 mod frame;
-mod music;
+mod decode;
+mod encode;
 
 #[tokio::main]
 async fn main() {
     println!("Howdy.");
 
-	music::decode_music(File::open("audio_samples/skyrim from past to present.mp3").expect("File open error"));
+	let decode_task = tokio::spawn(
+		decode::decode_music(
+			File::open("audio_samples/skyrim from past to present.mp3")
+				.expect("File open error")
+	));
+
+	let encode_task = tokio::spawn(
+		encode::encode_music()
+	);
+
+	tokio::try_join!(decode_task, encode_task).expect("Decode/Encode & Send concurrency failed");
 
 	let client = reqwest::Client::new();
 
-	let _res = client.post("http://127.0.0.1:4242")
-		.body("PACKAGE!").send().await.expect("Something went wrong here...");
+	// let _res = client.post("http://127.0.0.1:4242")
+	// 	.body("PACKAGE!").send().await.expect("Something went wrong here...");
 }
-
