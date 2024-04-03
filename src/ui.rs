a6c3cd53a0501b640429af6cb386b25f8a0f5e47
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-04-03 20:32:52 +0000

    Added a dropdown to select server, and laid out ui outline.

--- src/ui.rs
+++ src/ui.rs
@@ -1,16 +1,23 @@
 use iced::{
 	executor,
-	widget::{button, container, horizontal_space, row},
+	widget::{
+		button, column, container, horizontal_space, pick_list, row, scrollable, vertical_space,
+	},
 	Application, Command, Theme,
 };
 
 // Struct for holding the app state.
-pub(crate) struct Minstrel;
+#[derive(Default)]
+pub(crate) struct Minstrel {
+	target_guild: Option<u64>,
+	saved_guilds: Vec<u64>,
+}
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub(crate) enum Message {
 	Connect,
 	Connected,
+	SelectGuild(u64),
 }
 
 impl Application for Minstrel {
@@ -20,25 +27,49 @@ impl Application for Minstrel {
 	type Flags = ();
 
 	fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-		(Self, Command::none())
+		(
+			Self {
+				saved_guilds: vec![0, 1, 2, 3, 4, 5],
+				..Default::default()
+			},
+			Command::none(),
+		)
 	}
 
 	fn title(&self) -> String {
 		String::from("Minstrel")
 	}
 
-	fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
-		match message {
+	fn update(&mut self, msg: Self::Message) -> iced::Command<Self::Message> {
+		match msg {
 			Message::Connect => Command::perform(crate::play(), |_| Message::Connected),
 			Message::Connected => Command::none(),
+			Message::SelectGuild(guild) => {
+				self.target_guild = Some(guild);
+				Command::none()
+			}
 		}
 	}
 
-	fn view(&self) -> iced::Element<Self::Message, Self::Theme, iced::Renderer> {
+	fn view(&self) -> iced::Element<Self::Message> {
+		// The connection set.
 		let con_btn = button("Connect!").on_press(Message::Connect);
+		let guild_lst = pick_list(
+			self.saved_guilds.as_slice(),
+			self.target_guild,
+			Message::SelectGuild,
+		);
 
-		let menu_bar = row![horizontal_space(), con_btn].padding(10);
+		let menu_bar = row![horizontal_space(), guild_lst, con_btn]
+			.padding(10)
+			.spacing(5);
+		let side_bar = column![vertical_space()];
+		let board = scrollable(vertical_space());
 
-		container(menu_bar).into()
+		let main_view = row![side_bar, board];
+
+		let page = column![menu_bar, main_view];
+
+		container(page).into()
 	}
 }
